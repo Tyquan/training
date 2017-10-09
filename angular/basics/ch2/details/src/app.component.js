6333febe098dc8b44072b2dc5c0d57eedcdad9b1
Tyquan Reddick <tyquanjreddick@gmail.com>
2017-10-09 12:01:38 -0400

    updated

--- angular/basics/ch2/details/src/app.component.js
+++ angular/basics/ch2/details/src/app.component.js
@@ -7,36 +7,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-// import { Books } from './mock-books';
+var mock_books_1 = require("./mock-books");
 var AppComponent = (function () {
     function AppComponent() {
-        this.bookList = [
-            {
-                "isbn": 9781786462084,
-                "title": "Laravel 5.x Cookbook",
-                "authors": "Alfred Nutile",
-                "published": "September 2016",
-                "description": "A recipe-based book to help you efficiently create amazing PHP-based applications with Laravel 5.x",
-                "coverImage": "https://d255esdrn735hr.cloudfront.net/sites/default/files/imagecache/ppv4_main_book_cover/B05517_MockupCover_Cookbook_0.jpg"
-            },
-            {
-                "isbn": 9781784396527,
-                "title": "Sitecore Cookbook for Developers",
-                "authors": "Yogesh Patel",
-                "published": "April 2016",
-                "description": "Over 70 incredibly effective and practical recipes to get you up and running with Sitecore development",
-                "coverImage": 'https://d255esdrn735hr.cloudfront.net/sites/default/files/imagecache/ppv4_main_book_cover/6527cov_.jpg'
-            },
-            {
-                "isbn": 9781783286935,
-                "title": "Sass and Compass Designers Cookbook",
-                "authors": "Bass Jobsen",
-                "published": "April 2016",
-                "description": "Over 120 practical and easy-to-understand recipes that explain how to use Sass and Compass to write efficient, maintainable, and reusable CSS code for your web development projects",
-                "coverImage": "https://d1ldz4te4covpm.cloudfront.net/sites/default/files/imagecache/ppv4_main_book_cover/I6935.jpg"
-            }
-        ];
+        this.bookList = mock_books_1.BOOKS;
     }
+    AppComponent.prototype.getBookDetails = function (isbn) {
+        var selectedBook = this.bookList.filter(function (book) { return book.isbn === isbn; });
+        this.selectedBook = selectedBook[0];
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
