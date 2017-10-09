71b046108f3ddc1c4ae62af79b9fd2c46bbae09b
Tyquan Reddick <tyquanjreddick@gmail.com>
2017-10-09 12:37:37 -0400

    added multiple components

---
+++ angular/basics/ch3/multiple-components/src/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Book } from './book';
+import { BOOKS } from './mock-books';
+
+@Component({
+	selector: 'books',
+	templateUrl: 'src/app.component.html'
+})
+
+export class AppComponent {
+	bookList: Book[] = BOOKS;
+	selectedBook: Book;
+
+	getBookDetails(isbn: number){
+		var selectedBook = this.bookList.filter(book => book.isbn === isbn);
+		this.selectedBook = selectedBook[0];
+	}
+}
